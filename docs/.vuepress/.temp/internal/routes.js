40b93cb4b2a4e6a83305d9239b6bf77857622ebc
nagarajsaravanan <ngrjsaravanan@gmail.com>
2026-09-16 04:24:46 +0530

    Add GitHub Pages deployment

--- docs/.vuepress/.temp/internal/routes.js
+++ docs/.vuepress/.temp/internal/routes.js
@@ -6,16 +6,3 @@ export const routes = Object.fromEntries([
   ["/node-js/", { loader: () => import(/* webpackChunkName: "node-js_index.html" */"C:/NGRJ/HUSTLE/Mission possible/simple-learning/docs/node-js/README.md"), meta: {"title":"NODE JS"} }],
   ["/404.html", { loader: () => import(/* webpackChunkName: "404.html" */"C:/NGRJ/HUSTLE/Mission possible/simple-learning/docs/.vuepress/.temp/pages/404.html.vue"), meta: {"title":""} }],
 ]);
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  __VUE_HMR_RUNTIME__.updateRoutes?.(routes)
-  __VUE_HMR_RUNTIME__.updateRedirects?.(redirects)
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept((m) => {
-    __VUE_HMR_RUNTIME__.updateRoutes?.(m.routes)
-    __VUE_HMR_RUNTIME__.updateRedirects?.(m.redirects)
-  })
-}
